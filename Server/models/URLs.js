da85eeceabfe7e8619a3ab82c35595aae82bc0aa
Noufel17 <jn_naili@esi.dz>
2022-03-09 12:15:01 +0100

    added data base schema and database connection

---
+++ Server/models/URLs.js
@@ -0,0 +1,24 @@
+// URL data format in json (for the mongo database)
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+// the model of URLs data in mongo db (le model de donnees xD)
+const urlSchema =new Schema({
+    _id:{
+        type:String
+    },
+    URL:{
+        type:String,
+        required:true
+    },
+    hash:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+//now we export the data model (the Schema)
+module.exports = URL = mongoose.model('URL',urlSchema);
--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose= require('mongoose');
+const bodyParser = require('body-parser');
+
+
+// import URL data model 
+
+
 // initializing express
 const app=express();
 
+// bodyParser to get data from data base into the app in json format
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//database key
+const db=require('./config/keys').mongoURL;
+
+// create connection to the database
+mongoose.connect(db)
+    .then(()=> console.log('mongoDB connected sucessfully'))
+    .catch((err)=> console.log(err));
+
 // path to test the server 
 app.get('/',(req,res)=>{
-    res.send('response');
+    res.send('response from server');
 });
 
 //initializing server port 
